b1ab9d29407db9aa7d3d80a8cd405b3b6d98514e
connorbarkr <connorbarkr@gmail.com>
2018-03-22 12:02:42 -0400

    Started bugfixing on adding canvas elements

--- content_script.js
+++ content_script.js
@@ -1,9 +1,31 @@
 function img_replace() {
-  var imgs = document.getElementsByTagName("img");
-  var src = "POTD_chick_3597497k.jpg";
-  for (img of imgs) {
-    var url = chrome.extension.getURL(src);
-    img.src = url;
+  let imgs = document.getElementsByTagName("img");
+      imgLen = imgs.length;
+      canvas = document.createElement('canvas'),
+      ctx = canvas.getContext('2d');
+  for (var width, height, imgData, newImg, pixel, pix, pixLen, imgId = 0; imgId < imgLen; imgId++) {
+    original = imgs[imgId];
+    width = original.offsetWidth || original.naturalWidth;
+    height = original.offsetHeight || original.naturalHeight;
+    if (width == 0 || height == 0) {
+      continue;
+    }
+    canvas.width = width;
+    canvas.height = height;
+    ctx.drawImage(original, 0, 0);
+    imgData = ctx.getImageData(0, 0, width, height);
+    console.log(imgData);
+    // pix = imgData.data;
+    // pixLen = pix.length;
+    // for (pixel = 0; pixel < pixLen; pixel += 4) {
+    //   pix[pixel + 2] = pix[pixel + 1] = pix[pixel] = (pix[pixel] + pix[pixel + 1] + pix[pixel + 2]) / 3;
+    // }
+    // ctx.putImageData(imgData, 0, 0);
+    // var replacement = new Image();
+    // replacement.src = canvas.toDataURL();
+    // ctx.clearRect(0, 0, width, height);
+    original.style.display = "none";
+    // original.parentNode.insertBefore(replacement, original);
   }
 }
 
