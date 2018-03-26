748bc773b6fd6b73248278981ec2d8e69a53c842
connorbarkr <connorbarkr@gmail.com>
2018-03-26 13:49:54 -0400

    Images now replaceable

--- content_script.js
+++ content_script.js
@@ -1,31 +1,25 @@
 function img_replace() {
-  let imgs = document.getElementsByTagName("img");
-      imgLen = imgs.length;
-      canvas = document.createElement('canvas'),
-      ctx = canvas.getContext('2d');
-  for (var width, height, imgData, newImg, pixel, pix, pixLen, imgId = 0; imgId < imgLen; imgId++) {
-    original = imgs[imgId];
-    width = original.offsetWidth || original.naturalWidth;
-    height = original.offsetHeight || original.naturalHeight;
+  let imgs = document.getElementsByTagName('img');
+  let imgLen = imgs.length;
+
+  for (var imgId = 0; imgId < imgLen; imgId++) {
+    let original = imgs[imgId];
+    let canvas = document.createElement('canvas')
+    let ctx = canvas.getContext('2d');
+    let width = original.offsetWidth || original.naturalWidth;
+    let height = original.offsetHeight || original.naturalHeight;\
     if (width == 0 || height == 0) {
       continue;
     }
     canvas.width = width;
     canvas.height = height;
-    ctx.drawImage(original, 0, 0);
-    imgData = ctx.getImageData(0, 0, width, height);
-    console.log(imgData);
-    // pix = imgData.data;
-    // pixLen = pix.length;
-    // for (pixel = 0; pixel < pixLen; pixel += 4) {
-    //   pix[pixel + 2] = pix[pixel + 1] = pix[pixel] = (pix[pixel] + pix[pixel + 1] + pix[pixel + 2]) / 3;
-    // }
-    // ctx.putImageData(imgData, 0, 0);
-    // var replacement = new Image();
-    // replacement.src = canvas.toDataURL();
-    // ctx.clearRect(0, 0, width, height);
-    original.style.display = "none";
-    // original.parentNode.insertBefore(replacement, original);
+    // ctx.drawImage(original, 0, 0, width, height);
+    ctx.fillStyle = '#ff0000';
+    ctx.fillRect(0, 0, width, height);
+    let testImg = new Image();
+    let src = canvas.toDataURL();
+    original.src = src;
+    original.srcset = src;
   }
 }
 
