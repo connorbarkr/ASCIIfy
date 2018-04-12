4bdba572c8d230ad1abea68d037153e257bc8dab
connorbarkr <connorbarkr@gmail.com>
2018-04-11 21:44:47 -0400

    As good as it's gonna get

--- content_script.js
+++ content_script.js
@@ -4,41 +4,9 @@ function getRGB(data, index) {
 };
 
 function g2a(grayscale) {
-  switch (Math.floor(grayscale * 10) / 10) {
-    case (0.0):
-      return '@';
-      break;
-    case (0.1):
-      return '%';
-      break;
-    case (0.2):
-      return '#';
-      break;
-    case (0.3):
-      return '*';
-      break;
-    case (0.4):
-      return '+';
-      break;
-    case (0.5):
-      return '=';
-      break;
-    case (0.6):
-      return '-';
-      break;
-    case (0.7):
-      return ':';
-      break;
-    case (0.8):
-      return '.';
-      break;
-    case (0.9):
-      return ' ';
-      break;
-    default:
-      return ' ';
-      break;
-  }
+  let index = Math.ceil(grayscale * 9);
+  let values = ['@', '%', '#', 'x', '+', '=', ':', '-', '.', ' '];
+  return values[index];
 }
 
 function asciify() {
@@ -53,14 +21,13 @@ function asciify() {
     let img = new Image();
     // some issue with image dimensions; maybe map actual dimensions to screen dimensions?
     img.src = original.src;
-    console.log("Load: ", img.width, img.height);
     if (original) {
       src = document.getElementsByTagName('img')[imgId].getAttribute('src');
       jqObj = 'img[src="' + src + '"]';
     } else {
       continue;
     }
-    let jqRet = '<textarea id="ta' + imgId.toString() + '" style="border: none; resize: none; color: black; font-family: Courier; font-size: 7px; line-height: 1em; padding: 0px; ';
+    let jqRet = '<textarea id="ta' + imgId.toString() + '" style="border: none; resize: none; color: black; font-family: Courier; font-size: 4px; line-height: 1em; padding: 0px; ';
     let width = original.offsetWidth || original.naturalWidth;
     let height = original.offsetHeight || original.naturalHeight;
     let ratio = width / height;
@@ -86,8 +53,8 @@ function contentConstructor(src, width, height) {
     img.crossOrigin = 'Anonymous';
     img.src = src;
     //specific nums due to specific font size
-    let cols = Math.floor(width / 4.2);
-    let rows = Math.floor(height / 7.03);
+    let cols = Math.floor(width / 2.459);
+    let rows = Math.floor(height / 4.081);
     let cellWidth = Math.floor(width / cols);
     let cellHeight = Math.floor(height / rows);
     let canvas = document.createElement('canvas');
@@ -101,12 +68,11 @@ function contentConstructor(src, width, height) {
         for (var j = 0; j < cols - 1; j++) {
           ctx.drawImage(img, j * cellWidth, i * cellHeight, (j * cellWidth) + cellWidth, (i * cellHeight) + cellHeight, 0, 0, cellWidth, cellHeight);
           imgData = ctx.getImageData(0, 0, cellWidth, cellHeight).data;
-          console.log(imgData.length / 4);
           let len = cellWidth * cellHeight;
           let total = 0;
           for (var k = 0; k < len; k++) {
             let rgb = getRGB(imgData, k);
-            total += (((rgb[0]) + (rgb[1]) + (rgb[2])) / 3) / 255;
+            total += ((rgb[0] * 0.299) + (rgb[1] * 0.587) + (rgb[2] * 0.114)) / 255;
           }
           total = total / len;
           ret += g2a(total);
