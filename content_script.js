6284ebd1e337196efb9624dc8910b140588e993e
connorbarkr <connorbarkr@gmail.com>
2018-04-11 21:01:04 -0400

    Semi-functional; more like depth maps, need to improve image logic

--- content_script.js
+++ content_script.js
@@ -4,9 +4,41 @@ function getRGB(data, index) {
 };
 
 function g2a(grayscale) {
-  let chars = ['@','#','$','=','*','!',';',':','~','-',',','.','&nbsp;', '&nbsp;'];
-	let charLen = chars.length - 1;
-  return chars[parseInt(grayscale * charLen, 10)];
+  switch (Math.floor(grayscale * 10) / 10) {
+    case (0.0):
+      return '@';
+      break;
+    case (0.1):
+      return '%';
+      break;
+    case (0.2):
+      return '#';
+      break;
+    case (0.3):
+      return '*';
+      break;
+    case (0.4):
+      return '+';
+      break;
+    case (0.5):
+      return '=';
+      break;
+    case (0.6):
+      return '-';
+      break;
+    case (0.7):
+      return ':';
+      break;
+    case (0.8):
+      return '.';
+      break;
+    case (0.9):
+      return ' ';
+      break;
+    default:
+      return ' ';
+      break;
+  }
 }
 
 function asciify() {
@@ -18,13 +50,17 @@ function asciify() {
     let original = imgs[imgId];
     let src = '';
     let jqObj = '';
+    let img = new Image();
+    // some issue with image dimensions; maybe map actual dimensions to screen dimensions?
+    img.src = original.src;
+    console.log("Load: ", img.width, img.height);
     if (original) {
       src = document.getElementsByTagName('img')[imgId].getAttribute('src');
       jqObj = 'img[src="' + src + '"]';
     } else {
       continue;
     }
-    let jqRet = '<textarea id="ta' + imgId.toString() + '" style="resize: none; color: black; font-family: Courier; font-size: 7px; line-height: 1em; padding: 0px; ';
+    let jqRet = '<textarea id="ta' + imgId.toString() + '" style="border: none; resize: none; color: black; font-family: Courier; font-size: 7px; line-height: 1em; padding: 0px; ';
     let width = original.offsetWidth || original.naturalWidth;
     let height = original.offsetHeight || original.naturalHeight;
     let ratio = width / height;
@@ -63,17 +99,14 @@ function contentConstructor(src, width, height) {
       console.log("New image");
       for (var i = 0; i < rows; i++) {
         for (var j = 0; j < cols - 1; j++) {
-          let canvas = document.createElement('canvas');
-          let ctx = canvas.getContext('2d');
-          canvas.width = cellWidth;
-          canvas.height = cellHeight;
           ctx.drawImage(img, j * cellWidth, i * cellHeight, (j * cellWidth) + cellWidth, (i * cellHeight) + cellHeight, 0, 0, cellWidth, cellHeight);
           imgData = ctx.getImageData(0, 0, cellWidth, cellHeight).data;
+          console.log(imgData.length / 4);
           let len = cellWidth * cellHeight;
           let total = 0;
           for (var k = 0; k < len; k++) {
             let rgb = getRGB(imgData, k);
-            total += Math.max(rgb[0] * 0.3, rgb[1] * 0.59, rgb[2] * 0.11) / 255;
+            total += (((rgb[0]) + (rgb[1]) + (rgb[2])) / 3) / 255;
           }
           total = total / len;
           ret += g2a(total);
